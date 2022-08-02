99c4d41aa0a57d82c8c2d46090125a3467b48c57
Dmitriy <xxlaimxx1999@gmail.com>
2022-08-03 00:41:42 +0300

    feat: validate user

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,9 +1,24 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User, UserSchema } from './users.schema';
+import { MESSAGES } from '@nestjs/core/constants';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory() {
+          const schema = UserSchema;
+          schema.plugin(require('mongoose-unique-validator'));
+          return schema;
+        },
+      },
+    ]),
+  ],
 })
 export class UsersModule {}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './users.schema';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return await user.save();
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const users = await this.userModel.find();
+    return users;
   }
 
   findOne(id: number) {
